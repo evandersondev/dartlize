57ae01c8005a02f631e60fb2dc85f7f9eb825cfc
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2025-03-22 14:53:48 -0300

    discontinued

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Discontinued package.
+
 ## 0.0.1
 
 - Initial version of the package with sqlite support.
--- lib/dartlize.dart
+++ lib/dartlize.dart
@@ -3,17 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/new/query_builder.dart';
-// export 'src/new/dartonic.dart';
-// export 'src/new/driver.dart';
-// export 'src/new/mysql_driver_impl.dart';
-// export 'src/new/postgres_driver_impl.dart';
-// export 'src/new/sqlite_driver_impl.dart';
-export 'src/new/table.dart';
-// export 'src/dartlize_base.dart';
-// export 'src/data_types.dart';
-// export 'src/database_driver.dart';
-// export 'src/drivers/sqlite_driver.dart';
-// export 'src/model.dart';
+export 'src/dartlize_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
